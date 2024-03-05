954539707078ab3d3c582427662824cb85001069
chernetGit <henok.gebresenbet@gmail.com>
2024-03-05 10:35:01 +0200

    create contact and form validation

--- app/components/buttons.tsx
+++ app/components/buttons.tsx
@@ -1,6 +1,11 @@
-import Link from "next/link";
-import { IoAddSharp } from "react-icons/io5";
+"use client";
 
+import Link from "next/link";
+import { IoAddSharp, IoPencil, IoTrashOutline } from "react-icons/io5";
+import { useFormStatus } from "react-dom";
+import clsx from "clsx";
+
+//create button
 export const CreateButton = () => {
   return (
     <Link
@@ -10,5 +15,48 @@ export const CreateButton = () => {
       <IoAddSharp size={20} />
       Create
     </Link>
+  );
+};
+
+//edit button
+export const EditButton = () => {
+  return (
+    <Link
+      href="/contacts/edit"
+      className="rounded-sm border p-1 hover:bg-gray-100"
+    >
+      <IoPencil size={20} />
+    </Link>
+  );
+};
+
+//delete button
+export const DeleteButton = () => {
+  return (
+    <button className="rounded-sm border p-1 hover:bg-gray-100">
+      <IoTrashOutline size={20} />
+    </button>
+  );
+};
+
+//submit button
+export const SubmitButton = ({ label }: { label: string }) => {
+  const { pending } = useFormStatus();
+
+  const className = clsx(
+    "text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-sm text-sm w-full px-5 py-3 text-center",
+    {
+      "opacity-50 cursor-progress": pending,
+    }
+  );
+
+  return (
+    <button type="submit" className={className} disabled={pending}>
+      {label === "save" ? (
+        <span>{pending ? "Saving..." : "Save"}</span>
+      ) : (
+        <span>{pending ? "Updating..." : "Update"}</span>
+      )}
+    </button>
   );
 };
